1fe2bcf0fa85530089ceb698270b55c9af20ae0b
ZickZackZiege <34478115+ZickZackZiege@users.noreply.github.com>
2018-01-26 18:10:09 +0100

    Update Strings.java

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -28,9 +28,64 @@ public class Strings
        v[10] = "Bums";
        
        String[] n = new String[11];
-       
-       
-       
+       n[0] = "den Hund";
+       n[1] = "ihn";
+       n[2] = "den Diener";
+       n[3] = "den Knilch";
+       n[4] = "den Knecht";
+       n[5] = "den Enaj";
+       n[6] = "die Sau";
+       n[7] = "die fette Sau";
+       n[8] = "das Böse";
+       n[9] = "den Feigling";
+       n[10] = "den Fiesling";
         
+        String[] a = new String[11];
+        a[0] = "hart";
+        a[1] = "heftig";
+        a[2] = "gewalttätig";
+        a[3] = "sofort";
+        a[4] = "geisteskrank";
+        a[5] = "sofort";
+        a[6] = "mit Gewalt";
+        a[7] = "kräftig";
+        a[8] = "respektvoll";
+        a[9] = "sauber";
+        a[10] = "lecker";
+        
+        String[] b = new String[11];
+        b[0] = "du Ziggy";
+        b[1] = "junger Padawan";
+        b[2] = "du Maschine";
+        b[3] = "du Biest";
+        b[4] = "du Tier";
+        b[5] = "Retter des Reiches";
+        b[6] = "Jungspund";
+        b[7] = "du Krieger";
+        b[8] = "Held";
+        b[9] = "Knechter des Präsidenten";
+        b[10] = "long dong Silver";
+
+        String r = v[(int)(Math.random()*10)]+" "+n[(int)(Math.random()*10)]+" "+a[(int)(Math.random()*10)]+", "+b[(int)(Math.random()*10)];    
+        
+        String[] s = new String[15];
+        s[0] = "Herzlichen Glückwunsch, Ziggy. Du hast soeben die beste App des gesamten Playstores gezogen."
+        s[1] = "Doch du kommst zu einer ungünstigen Zeit";
+        s[2] = "In letzter Zeit hat unser geliebtes Ziegenreich unter der babarischen Dynastie des autoritären, im Osten herrschenden Präsidenten E. gelitten";
+        s[3] = "Dieser Babar hat mit seinem gefürchteten Säbel und seinen Liebeskünsten viele unserer besten Ziegenkrieger gefi..., sorry gefoltert und sie so auf seine Seite gezogen";
+        s[4] = "Mein Freund, nun ist es an dir Zigistan zu retten. Doch zuerst müssen wir aus diesem Gefängnis entkommen. Nutze folgendes mächtiges Werkzeug um deine Feinde zu unterjochen."
+        s[5] = "Dies ist der Rammbock. Er ist zwar die schwächste, aber auch die am einfachsten anzuwendende Waffe gegen deine Gegner";
+        s[6] = "Er ist so anwendbar";
+        s[7] = "Oder auch so";
+        s[8] = "Aber OMG auch senkrecht kann er deine Gegner durchdringen";
+        s[9] = "Nutze den Rammbock um die Tür zu durchbrechen und aus dem Gefängnis zu entfliehen, um den Dichtern und Oppositionellen in diesem Land wieder Hoffnung zu schenken";
+        // erstes Game
+        s[10] = "Oh hell no. Dein Vater. Er ist in die schmierigen Finger Es gelangt und wird von einem jungen, brutalen und gutaussehenden Ziegentürstehers bewacht";
+        s[11] = "Doch ein Vögelchen hat mir gezwitchert, dass dieser Diener dem Koka.., dem Geld mehr zugeneigt ist als der Treue";
+        s[12] = "Bist du bereit für die nächste Lektion? Die korrupte Ziege";
+        s[13] = "Sie ist wie der Rammbock senkrecht, waagerecht und so weiter anwendbar"
+        s[14] = "Was stehst du hier noch so dumm rum. Nutze den Rammbock und die korrupte Ziege um deinen Vater zu befreien";
+        
+
     }
 }
