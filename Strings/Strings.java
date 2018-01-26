02f3a7bbf46047af16e64ad1bafc30d3203cdfd0
ZickZackZiege <34478115+ZickZackZiege@users.noreply.github.com>
2018-01-26 09:33:04 +0100

    Add files via upload

---
+++ Strings/Strings.java
@@ -0,0 +1,36 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Strings.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Strings
+{
+    // Instanzvariablen 
+
+    /**
+     * Konstruktor für Objekte der Klasse Strings
+     */
+    public Strings()
+    {
+       String[] v = new String[11];
+       v[0] = "Attakiere";
+       v[1] = "Zerstöre";
+       v[2]= "Zerberste";
+       v[3] = "Natze";
+       v[4] = "Massakriere";
+       v[5] = "Fi.. Verzeihung Foltere";
+       v[6] = "Stich";
+       v[7] = "Bekriege";
+       v[8] = "Bekämpfe";
+       v[9] = "Vernichte";
+       v[10] = "Bums";
+       
+       String[] n = new String[11];
+       
+       
+       
+        
+    }
+}
