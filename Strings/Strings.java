a711a071f8761ad4930942836aeedc7f93ca9c38
ZickZackZiege <34478115+ZickZackZiege@users.noreply.github.com>
2018-01-26 18:30:46 +0100

    Update Strings.java

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -68,12 +68,12 @@ public class Strings
 
         String r = v[(int)(Math.random()*10)]+" "+n[(int)(Math.random()*10)]+" "+a[(int)(Math.random()*10)]+", "+b[(int)(Math.random()*10)];    
         
-        String[] s = new String[15];
-        s[0] = "Herzlichen Glückwunsch, Ziggy. Du hast soeben die beste App des gesamten Playstores gezogen."
+        String[] s = new String[30];
+        s[0] = "Herzlichen Glückwunsch, Ziggy. Du hast soeben die beste App des gesamten Playstores gezogen.";
         s[1] = "Doch du kommst zu einer ungünstigen Zeit";
         s[2] = "In letzter Zeit hat unser geliebtes Ziegenreich unter der babarischen Dynastie des autoritären, im Osten herrschenden Präsidenten E. gelitten";
         s[3] = "Dieser Babar hat mit seinem gefürchteten Säbel und seinen Liebeskünsten viele unserer besten Ziegenkrieger gefi..., sorry gefoltert und sie so auf seine Seite gezogen";
-        s[4] = "Mein Freund, nun ist es an dir Zigistan zu retten. Doch zuerst müssen wir aus diesem Gefängnis entkommen. Nutze folgendes mächtiges Werkzeug um deine Feinde zu unterjochen."
+        s[4] = "Mein Freund, nun ist es an dir Zigistan zu retten. Doch zuerst müssen wir aus diesem Gefängnis entkommen. Nutze folgendes mächtiges Werkzeug um deine Feinde zu unterjochen.";
         s[5] = "Dies ist der Rammbock. Er ist zwar die schwächste, aber auch die am einfachsten anzuwendende Waffe gegen deine Gegner";
         s[6] = "Er ist so anwendbar";
         s[7] = "Oder auch so";
@@ -83,8 +83,26 @@ public class Strings
         s[10] = "Oh hell no. Dein Vater. Er ist in die schmierigen Finger Es gelangt und wird von einem jungen, brutalen und gutaussehenden Ziegentürstehers bewacht";
         s[11] = "Doch ein Vögelchen hat mir gezwitchert, dass dieser Diener dem Koka.., dem Geld mehr zugeneigt ist als der Treue";
         s[12] = "Bist du bereit für die nächste Lektion? Die korrupte Ziege";
-        s[13] = "Sie ist wie der Rammbock senkrecht, waagerecht und so weiter anwendbar"
+        s[13] = "Sie ist wie der Rammbock senkrecht, waagerecht und so weiter anwendbar";
         s[14] = "Was stehst du hier noch so dumm rum. Nutze den Rammbock und die korrupte Ziege um deinen Vater zu befreien";
+        // zweites Game
+        s[15] = "Sohnemann, du hast mich gerettet. Ich dachte schon, dass ich dich nie mehr erblicken würde";
+        s[16] = "Lass uns schnell abhauen, bevor uns deine Mama oder ein Wächter entdeckt";
+        s[17] = "Was du willst deine Mama befreien? Krank.";
+        s[18] = "Du weißt aber schon, dass die von einem gefürchteten Ziegenkrieger bewacht wird";
+        s[19] = "Ok, Ok. Ist ja schon gut. Damit du nicht draufgehst, bringe ich dir meine spezielle Sonderfähigkeit bei.";
+        s[20] = "Schau her";
+        // drittes Game
+        s[21] = "Du hast es geschafft. Ich wusste schon immer, dass der Sturz vom Wickeltich dich nicht geistig beeinträchtigt hat";
+        s[22] = "Oh nein, dahinten kommt Präsident E.";
+        s[23] = "Schnell erlerne noch meine persönliche Waffe, sie ist die stärkste aller Gerätschaften und wird dir im Kampf der heiligen Ziege 7 Punkte einbringen";
+        s[24] = "Besonders wichtig bei dieser Form ist es, dass der Gegner umzingelt wird und ...";
+        // viertes Game
+        s[25] = "Du hast ihn besiegt. Du geisteskranker Lümmel. Das gesamte Ziegenreich ist am frohlocken. Hier nimm diese Geschenke";
+        s[26] ="Aber, Aber, das sind ja nur leere Umzugskartons";
+        s[27] = "Richtig du Dummkopf, da kannst du deine Sachen reinpacken und dich verpissen.";
+        s[28] = "Beeil dich, zieh in die Welt hinaus und Messe dich mit anderen Kriegern des Reiches";
+        
         
 
     }
